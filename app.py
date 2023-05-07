d1b68e09c3eefb0611e1cf26ee408a5f91664a81
Eric Julianto <algonacci1@gmail.com>
2023-05-07 16:38:45 +0700

    Finishing

--- app.py
+++ app.py
@@ -5,9 +5,7 @@ import numpy as np
 from sklearn.decomposition import PCA
 
 app = Flask(__name__)
-df_processed = pd.read_csv("static/data.csv")
 pca = PCA(n_components=3)
-df_pca = pca.fit_transform(df_processed)
 
 m = 2
 n_clusters = 3
@@ -22,7 +20,19 @@ def index():
 
 @app.route("/in_scope")
 def in_scope():
-    return render_template("in_scope.html")
+    df1 = pd.read_excel("data/Dataset_oktober.xlsx")
+    df2 = pd.read_excel("data/Dataset_november.xlsx")
+    df3 = pd.read_excel("data/Dataset_desember.xlsx")
+    data_path = md.preprocess_df(df1, df2, df3)
+
+    df_pca = pca.fit_transform(pd.read_csv(data_path))
+    centers, membership_matrix = md.fuzzy_c_means(
+        df_pca, n_clusters, m, epsilon=epsilon, max_iter=max_iter)
+    cluster_membership = np.argmax(membership_matrix, axis=1)
+    viz = md.visualization(cluster_membership, centers)
+    cluster_counts = pd.Series(
+        cluster_membership).value_counts().sort_index().to_dict()
+    return render_template("cluster_in_scope.html", viz=viz, cluster_counts=cluster_counts)
 
 
 @app.route("/cluster_non_scope", methods=["POST"])
@@ -40,23 +50,6 @@ def cluster_non_scope():
     cluster_counts = pd.Series(
         cluster_membership).value_counts().sort_index().to_dict()
     return render_template("cluster_non_scope.html", viz=viz, cluster_counts=cluster_counts)
-
-
-@app.route("/cluster_in_scope", methods=["POST"])
-def cluster_in_scope():
-    df1 = pd.read_excel(request.files["file1"])
-    df2 = pd.read_excel(request.files["file2"])
-    df3 = pd.read_excel(request.files["file3"])
-    data_path = md.preprocess_df(df1, df2, df3)
-
-    df_pca = pca.fit_transform(pd.read_csv(data_path))
-    centers, membership_matrix = md.fuzzy_c_means(
-        df_pca, n_clusters, m, epsilon=epsilon, max_iter=max_iter)
-    cluster_membership = np.argmax(membership_matrix, axis=1)
-    viz = md.visualization(cluster_membership, centers)
-    cluster_counts = pd.Series(
-        cluster_membership).value_counts().sort_index().to_dict()
-    return render_template("cluster_in_scope.html", viz=viz, cluster_counts=cluster_counts)
 
 
 if __name__ == "__main__":
